edaaf6de1d0d0885f5b0422f77d6794a719dcb15
João Andrade <andrade.fasters@singulare.com.br>
2021-09-30 02:54:27 -0300

    react app criado

---
+++ src/components/Products/Products.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Grid from '@material-ui/core';
+
+class Products extends Component {
+  render() {
+    return (
+      <div>
+        <Grid container justify="center" spacing={4}>
+
+        </Grid>
+      </div>
+    );
+  }
+}
+
+export default Products;
